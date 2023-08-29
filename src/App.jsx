4be794a1ae49bacd44a0c334708cef280a9dbc0c
202208521-luis-morales <3683798410101@ingenieria.usac.edu.gt>
2023-08-28 19:27:54 -0600

    Vista terminada
    Falta conectarlo al backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,146 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from "react";
+import "./App.css";
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
+  const MySwal = withReactContent(Swal);
+
+  function onUserFormSubmit() {
+    MySwal.fire(
+      'Usuario agregado',
+      'El usuario ha sido agregado con éxito',
+      'success'
+    )
+  }
+
+  useEffect(() => {
+    document.title = "CRUD Usuarios | Grupo #5";
+  });
 
   return (
-    <>
+    <div className="container mt-5 d-flex row-gap-2 flex-column">
+      <h1 className="text-center">CRUD Usuarios</h1>
+      <h3 className="text-center">Grupo #5</h3>
+      <div className="d-flex justify-content-between column-gap-4">
+        <div className="w-50">
+          <div className="card w-75">
+            <div className="text-center card-header">
+              <h4>Agregar Usuario</h4>
+            </div>
+            <div className="card-body">
+              <form onSubmit={onUserFormSubmit}>
+                <div className="mb-3">
+                  <label className="form-label" htmlFor="name">
+                    Nombre
+                  </label>
+                  <input
+                    autoComplete="off"
+                    className="form-control"
+                    type="text"
+                    id="name"
+                  />
+                </div>
+                <div className="mb-3">
+                  <label className="form-label" htmlFor="lastname">
+                    Apellido
+                  </label>
+                  <input
+                    autoComplete="off"
+                    className="form-control"
+                    type="text"
+                    id="lastname"
+                  />
+                </div>
+                <div className="mb-3">
+                  <label className="form-label" htmlFor="age">
+                    Edad
+                  </label>
+                  <input
+                    autoComplete="off"
+                    className="form-control"
+                    type="number"
+                    id="age"
+                  />
+                </div>
+                <div className="mb-3">
+                  <label className="form-label" htmlFor="email">
+                    Email
+                  </label>
+                  <input
+                    autoComplete="off"
+                    className="form-control"
+                    type="email"
+                    id="email"
+                  />
+                </div>
+                <div className="text-center">
+                  <button type="submit" className="btn btn-primary">
+                    + Agregar Usuario
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+        <div className="w-50">
+          <div className="card">
+            <div className="card-header text-center">
+              <h4>Ver Usuario</h4>
+            </div>
+            <div className="card-body text-center text-body-secondary">
+              Elije un usuario y se mostrará aquí
+            </div>
+          </div>
+        </div>
+      </div>
+      <hr />
+      <div className="text-center">
+        <h2>Tabla Usuarios</h2>
+      </div>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <table className="table table-bordered">
+          <thead className="table-dark">
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Nombre</th>
+              <th scope="col">Apellido</th>
+              <th scope="col">Edad</th>
+              <th scope="col">Email</th>
+              <th scope="col">Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>Luis</td>
+              <td>Morales</td>
+              <td>20</td>
+              <td>mi@email.com</td>
+              <td className="d-flex justify-content-center">
+                <div
+                  className="btn-group"
+                  role="group"
+                  aria-label="Basic example"
+                >
+                  <button type="button" className="btn btn-secondary">
+                    Ver
+                  </button>
+                  <button type="button" className="btn btn-primary">
+                    Editar
+                  </button>
+                  <button type="button" className="btn btn-danger">
+                    Eliminar
+                  </button>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      'bootstrap/dist/js': 'bootstrap/dist/js'
+    }
+  },
+  optimizeDeps: {
+    include: ['bootstrap']
+  }
 })
